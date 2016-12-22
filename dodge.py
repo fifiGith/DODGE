11454550a5f783cc5247d09804ad634f8c1411ab
fifiGith <fordfifi@hotmail.com>
2016-12-22 14:42:42 +0700

    first commit

---
+++ dodge.py
@@ -0,0 +1,20 @@
+import arcade
+
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 600
+
+class GameWindow(arcade.Window):
+    def __init__(self, width, height):
+        super().__init__(width, height)
+
+        arcade.set_background_color(arcade.color.BLACK) 
+ 
+    def on_draw(self):
+        arcade.start_render()
+ 
+def main():
+	window = GameWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
+	arcade.run()
+
+if __name__ == '__main__':
+	main()
