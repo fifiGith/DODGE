399d6321ed78590aabbafac06f12fd3f0385db02
fifiGith <fordfifi@hotmail.com>
2016-12-22 15:33:14 +0700

    Test adding object

--- dodge.py
+++ dodge.py
@@ -4,13 +4,26 @@ SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 600
 
 class GameWindow(arcade.Window):
-    def __init__(self, width, height):
-        super().__init__(width, height)
+	def __init__(self, width, height):
+		super().__init__(width, height)
 
-        arcade.set_background_color(arcade.color.BLACK) 
- 
-    def on_draw(self):
-        arcade.start_render()
+		arcade.set_background_color(arcade.color.BLACK)
+
+		self.bullet = Bullet(30, 30)
+
+	def on_draw(self):
+		arcade.start_render()
+		self.bullet.draw()
+		
+
+class Bullet:
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
+		self.circle_radius = 20
+	
+	def draw(self):
+		arcade.draw_circle_filled(self.x, self.y, 30, arcade.color.WHITE)
  
 def main():
 	window = GameWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
