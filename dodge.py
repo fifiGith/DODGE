ba35f394539407e4421279244a63d73c12a6fb6a
fifiGith <fordfifi@hotmail.com>
2016-12-22 16:24:31 +0700

    optimized

--- dodge.py
+++ dodge.py
@@ -10,11 +10,13 @@ class GameWindow(arcade.Window):
 
 		arcade.set_background_color(arcade.color.BLACK)
 
-		self.bullet = Bullet("up")
+		self.bullet_list = []
 
 	def on_draw(self):
 		arcade.start_render()
-		self.bullet.draw()
+		self.bullet_list.append(Bullet("left"))
+		for bullet in self.bullet_list:
+			bullet.draw()
 		
 
 class Bullet:
@@ -22,8 +24,8 @@ class Bullet:
 		self.dir = dir
 		self.x = random.randrange(0, SCREEN_WIDTH)
 		self.y = random.randrange(0, SCREEN_HEIGHT)
+		self.circle_radius = 3
 		self.speed = 5
-		self.circle_radius = 20
 	
 	def update(self):
 		if self.dir == "left":
@@ -36,8 +38,16 @@ class Bullet:
 			self.y += self.speed
 
 	def draw(self):
-		self.update();
-		arcade.draw_circle_filled(self.x, self.y, 30, arcade.color.WHITE)
+		self.stop_update = False
+		
+		if self.stop_update == False:
+	
+			if self.x < 0 or self.x > SCREEN_WIDTH or self.y < 0 or self.y > SCREEN_HEIGHT:
+				self.stop_update = True
+	
+			if self.stop_update == False:
+				self.update();
+				arcade.draw_circle_filled(self.x, self.y, self.circle_radius, arcade.color.WHITE)
  
 def main():
 	window = GameWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
