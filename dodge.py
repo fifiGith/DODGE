f5210a57538c07cecbe2afbedb17a1b0bd591cef
fifiGith <fordfifi@hotmail.com>
2016-12-22 17:24:43 +0700

    add 4 directions

--- dodge.py
+++ dodge.py
@@ -11,19 +11,31 @@ class GameWindow(arcade.Window):
 		arcade.set_background_color(arcade.color.BLACK)
 
 		self.bullet_list = []
+		self.dir = 0
 
 	def on_draw(self):
 		arcade.start_render()
-		self.bullet_list.append(Bullet("left"))
+		self.dir = RandomDir()
+		self.bullet_list.append(Bullet(self.dir))
 		for bullet in self.bullet_list:
 			bullet.draw()
-		
 
 class Bullet:
 	def __init__(self, dir):
 		self.dir = dir
-		self.x = random.randrange(0, SCREEN_WIDTH)
-		self.y = random.randrange(0, SCREEN_HEIGHT)
+		if self.dir == "left":
+			self.x = 0
+			self.y = random.randrange(0, SCREEN_HEIGHT)
+		elif self.dir == "right":
+			self.x = SCREEN_WIDTH
+			self.y = random.randrange(0, SCREEN_HEIGHT)
+		elif self.dir == "up":
+			self.x = random.randrange(0, SCREEN_WIDTH)
+			self.y = SCREEN_HEIGHT
+		elif self.dir == "down":
+			self.x = random.randrange(0, SCREEN_WIDTH)
+			self.y = 0
+
 		self.circle_radius = 3
 		self.speed = 5
 	
@@ -49,6 +61,17 @@ class Bullet:
 				self.update();
 				arcade.draw_circle_filled(self.x, self.y, self.circle_radius, arcade.color.WHITE)
  
+def RandomDir():
+	n = random.randrange(0, 4)
+	if n == 0:
+		return "left"
+	elif n == 1:
+		return "right"
+	elif n == 2:
+		return "up"
+	elif n == 3:
+		return "down"
+
 def main():
 	window = GameWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
 	arcade.run()
